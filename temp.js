44257123a5b1003f90d7da9995934ab58c54ea2d
founderqiang <founderqiang@gmail.com>
2023-02-19 20:39:08 +0800

    new temp

--- temp.js
+++ temp.js
@@ -10,8 +10,12 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(() => {
     'use strict';
-
-    console.log('hello new world');
+    let a = document.querySelector('.item-thumb');
+    a.style.backgroundImage='url(/usr/customize/logo.png)';
+    a.addEventListener('click',() => {
+        a.style.background='#f0f';
+    });
+    // Your code here...
 })();
