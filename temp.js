cdc0dffc8415f26017f9985a526f178c2898807d
founderqiang <founderqiang@gmail.com>
2023-02-19 20:48:28 +0800

    new temp

--- temp.js
+++ temp.js
@@ -1,12 +1,10 @@
 // ==UserScript==
-// @name         hello world
-// @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  try to take over the world!
-// @author       You
-// @match        *://*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
-// @license      AGPL License
+// @name         第一个油猴脚本
+// @namespace    http://blog.chrxw.com/
+// @version      0.1
+// @description  hello world
+// @author       Chr_
+// @match        https://blog.chrxw.com/about.html
 // @grant        none
 // ==/UserScript==
 
